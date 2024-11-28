0884594b9fe21c37afb8ff2b329211250ade99de
kyungbin <thrudqls13@naver.com>
2024-11-28 09:45:33 +0900

    로딩까지 포함하고 한번에 처리

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,22 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+    reactStrictMode: true,
+    async rewrites() {
+        return [
+            {
+                source: "/makeup/:path*",
+                destination: `${process.env.NEXT_PUBLIC_MAKEUP_API_BASE_URL}/:path*`, // Makeup API 경로
+            },
+            {
+                source: "/guestbook/:path*",
+                destination: `${process.env.NEXT_PUBLIC_LOCAL_API_BASE_URL}/guestbook/:path*`, // Guestbook API 경로
+            },
+            {
+                source: "/members/:path*",
+                destination: `${process.env.NEXT_PUBLIC_LOCAL_API_BASE_URL}/members/:path*`, // Members API 경로
+            },
+        ];
+    }
+};
 
 export default nextConfig;
